b23f30d8c2b1e284cc407060e85ace6bee34159e
RIADWaffle <106703366+RIADWaffle@users.noreply.github.com>
2023-07-31 12:11:05 -0600

    Update Enfermedad.cs
    commentary

--- Perdivire v17/Assets/Scripts/Enfermedad.cs
+++ Perdivire v17/Assets/Scripts/Enfermedad.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
+
+// Class to get the info of the pathologies in the DB
 public class Enfermedad : MonoBehaviour
 {
-    public string parteCuerpo="";//nombre de la parte del cuerpo
+    public string parteCuerpo="";//Name of the body part to get all the pathologies info
     public Text textElement;
     public string valor;
+    
     void Start()
     {
         StartCoroutine(CorrutinaLeerSimple());
     }
 
-    private IEnumerator CorrutinaLeerSimple(){//corrutina
-        UnityWebRequest web = UnityWebRequest.Get("https://perdivire.000webhostapp.com/BaseDatos.php?parte=" + parteCuerpo);//accede a la base de datos a la parte del cuerpo solicitada
-        yield return web.SendWebRequest();// esperar al resultado de internet
+    private IEnumerator CorrutinaLeerSimple(){//corrutine for the start() method
+        UnityWebRequest web = UnityWebRequest.Get("https://perdivire.000webhostapp.com/BaseDatos.php?parte=" + parteCuerpo);// Access the body part info in the DB
+        yield return web.SendWebRequest();// Wait the response for the DB
 
-        if(web.downloadHandler.text == "mal"){//si ubo un error da lo imprime
-            Debug.Log("Error");//da un error en la consola
-        }else{//si no...
-
-            textElement.text=web.downloadHandler.text;//Imprimir la info en el text field
+        // ERROR connecting to the DB
+        if(web.downloadHandler.text == "mal"){
+            Debug.Log("Error");
+        }else{ // The DB was accesed correctly
+            textElement.text=web.downloadHandler.text;// Show the info
         }
     }
 }
