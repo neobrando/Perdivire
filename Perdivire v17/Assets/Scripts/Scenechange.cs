a7817d28ab3010410de95117890664760ee20c70
RIADWaffle <106703366+RIADWaffle@users.noreply.github.com>
2023-07-31 12:15:06 -0600

    Update Scenechange.cs
    comment

--- Perdivire v17/Assets/Scripts/Scenechange.cs
+++ Perdivire v17/Assets/Scripts/Scenechange.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+// Scene/frame changer for all the app
 public class Scenechange : MonoBehaviour
 {
     public void LoadScene(string sceneName)
