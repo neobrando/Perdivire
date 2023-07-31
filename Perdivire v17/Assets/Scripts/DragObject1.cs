03925295dbdfe4f5337f8163a7a26adac714e7e9
RIADWaffle <106703366+RIADWaffle@users.noreply.github.com>
2023-07-31 12:06:50 -0600

    Update DragObject1.cs
    commentary

--- Perdivire v17/Assets/Scripts/DragObject1.cs
+++ Perdivire v17/Assets/Scripts/DragObject1.cs
@@ -3,18 +3,21 @@ using UnityEngine;
 
 
 public class DragObject1 : MonoBehaviour {
-
+ // Variables
  Vector3 dist;
  float posX;
  float posY;    
- 
+
+
+ // Function that detects when de 3D model is pressed
  void OnMouseDown(){
   dist = Camera.main.WorldToScreenPoint(transform.position);
   posX = Input.mousePosition.x - dist.x;
   posY = Input.mousePosition.y - dist.y;
   
  }
- 
+
+ // Function that moves the 3D object while being pressed
  void OnMouseDrag(){
   Vector3 curPos = 
    new Vector3(Input.mousePosition.x - posX, 
